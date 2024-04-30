cf26ec169b3b5fc2333a49e2848da93f22d7f05c
PEGutierrF <pgutierr@uvm.edu>
2024-04-30 13:40:17 -0400

    data

---
+++ Module 10 Factorial ANOVA/Code template.R
@@ -0,0 +1,27 @@
+
+
+
+
+# Code Template: NR-5990
+# Practice: Factorial ANOVA
+# Pablo E. Gutierrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# Tue Apr 30 13:16:20 2024 ------------------------------
+
+
+# The Bartlett test is used to test the null hypothesis that the variances are equal across 
+# different groups or levels of a categorical variable. 
+# Bartlett test for homogeneity of variances
+bartlett.test(indepen_var ~ indep_var1 + indep_var2 + indep_var1:indep_var2, data=your_data)
+print(bartlett_result)
+
+# Do the factorial ANOVA
+mod <- aov(depen_var ~ indep_var1 + indep_var2 + indep_var1:indep_var2, data = your_data)
+summary(mod)
+
+# Run post hoc tests - Tukey HSD
+tukey_result <- TukeyHSD(mod)
+print(tukey_result)
+
+# Graphical display - to plot CIs from Tukey
+plot(tukey_result)
