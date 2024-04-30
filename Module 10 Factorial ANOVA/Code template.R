f43116bd91fc6605e48672dd4109662f0a4e2fd0
PEGutierrF <pgutierr@uvm.edu>
2024-04-30 15:21:06 -0400

    data

--- Module 10 Factorial ANOVA/Code template.R
+++ Module 10 Factorial ANOVA/Code template.R
@@ -15,13 +15,19 @@
 bartlett.test(indepen_var ~ indep_var1 + indep_var2 + indep_var1:indep_var2, data=your_data)
 print(bartlett_result)
 
+
 # Do the factorial ANOVA
-mod <- aov(depen_var ~ indep_var1 + indep_var2 + indep_var1:indep_var2, data = your_data)
+# The asterisk (*) is use to indicate all main effects and interactions among the variables
+# that it joins. So, for example the term A*B would expand to the three terms 
+# indep_var1, indep_var2, and indep_var1:indep_var2.
+mod <- aov(depen_var ~ indep_var1 * indep_var2, data = your_data)
 summary(mod)
+
 
 # Run post hoc tests - Tukey HSD
 tukey_result <- TukeyHSD(mod)
 print(tukey_result)
+
 
 # Graphical display - to plot CIs from Tukey
 plot(tukey_result)
