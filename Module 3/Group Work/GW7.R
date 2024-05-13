cbe05be564932623b64d37eac5688bb5306a2cfa
PEGutierrF <pgutierr@uvm.edu>
2024-05-13 13:59:21 -0400

    data

---
+++ Module 3/Group Work/GW7.R
@@ -0,0 +1,28 @@
+
+#### NR2400 ####
+#### Group Work 7 ####
+
+
+#loading in the libraries we will use
+library(tidyverse)
+library(lessR)
+library(DT)
+library(e1071)
+library(readxl)
+library(stats)
+library(BSDA)
+
+nests <- read.csv("Laloë_et_al._2020_Ecol_Evol_dataset.csv")
+
+pivot(nests, c(mean, sd), nestTemperature)
+
+ggplot(nests, aes(x = nestTemperature)) + geom_density()
+
+yourNests <- read_xlsx("yourTurtleData.xlsx")
+pivot(nests, c(mean, sd), nestTemperature)
+
+z.test(x=yourNests$nestTemperature,
+       mu=31.2,
+       alternative = "two.sided",
+       sigma.x = 1.822)
+
