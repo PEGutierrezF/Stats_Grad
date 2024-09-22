e20745d8d0e77579a8467879e70a14811b4e2565
PEGutierrF <pgutierr@uvm.edu>
2024-09-21 22:03:21 -0400

    data

--- format.R
+++ format.R
@@ -6,12 +6,19 @@
 
 
 # Columns -----------------------------------------------------------------
+## Why do we care about	**skew** and **kurtosis**?
+- Because many statistical analyses assume a normal distribution of the data, testing for normality must always be a precursor to any analysis. 
+
 ::: columns
 :::: column
 
 ::::
-:::
+  
+:::: column
 
+::::
+:::
+  
 
 # Size --------------------------------------------------------------------
 \tiny
