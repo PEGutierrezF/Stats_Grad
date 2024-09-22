4e4fa2bc72872a70054dc88f4f0649fcb1a6ce45
PEGutierrezF <pabloe.gutierrezfonseca@gmail.com>
2024-09-22 15:02:20 -0400

    data

--- format.R
+++ format.R
@@ -9,15 +9,18 @@
 ## Why do we care about	**skew** and **kurtosis**?
 - Because many statistical analyses assume a normal distribution of the data, testing for normality must always be a precursor to any analysis. 
 
-::: columns
-:::: column
+:::::::::::::: {.columns}
+::: {.column width="50%"}
 
-::::
-  
-:::: column
+contents...
 
-::::
 :::
+::: {.column width="50%"}
+
+contents...
+
+:::
+::::::::::::::
   
 
 # Size --------------------------------------------------------------------
@@ -30,6 +33,10 @@
 # Photo -------------------------------------------------------------------
 ```{r, echo=FALSE, fig.cap="", out.width = '80%', fig.align='center'}
 knitr::include_graphics("fig/Histogram.jpg")
+```
+
+Figures
+```{r, echo=FALSE, message=FALSE, fig.cap="", fig.width=3, fig.height=4.5}
 ```
 
 
