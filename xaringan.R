42c5198e4221453c1aad44db93bcb9bc1fb10f96
PEGutierrF <pgutierr@uvm.edu>
2024-10-03 13:13:47 -0400

    class

--- xaringan.R
+++ xaringan.R
@@ -1,6 +1,14 @@
 
+```{r xaringan-logo, echo=FALSE}
+xaringanExtra::use_logo(
+  image_url = <fill in your path> )
+```
+
 
 class: center, middle
 
 .pull-left[ ]
 .pull-right[ ]
+
+
+`` hihglight yellow
