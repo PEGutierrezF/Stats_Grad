869ccbca6b35039c0b4f30da8e8262c116c89a7f
PEGutierrF <pgutierr@uvm.edu>
2024-10-07 14:23:03 -0400

    data

--- xaringan.R
+++ xaringan.R
@@ -17,6 +17,8 @@ class: center, middle
 </center>
 
 $\alpha+\beta$
+
+<img src="fig/t-table_1.png" alt="Degrees of Freedom T-Table" width="600"/>
   
 <span style="color:#00796B;">TEXT </span>
   $$\require{color}\displaystyle\colorbox{#FFD1DC}{$Y = \beta_0 + \beta_1 X_1 + \ldots + \beta_n X_n$}$$
