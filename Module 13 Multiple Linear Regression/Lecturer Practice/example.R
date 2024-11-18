1ad40ff6e224a1de1737d5b31dc38ae5ca5736f7
PEGutierrF <pgutierr@uvm.edu>
2024-11-18 13:18:37 -0500

    data

---
+++ Module 13 Multiple Linear Regression/Lecturer Practice/example.R
@@ -0,0 +1,55 @@
+
+
+data <- read.csv('heart.data.csv')
+head(data)
+
+mod <- lm(heart.disease ~ biking + smoking, data = data)
+summary(mod)
+
+# Equation
+heart disease = 15 + (-0.2*biking) + (0.178*smoking) ± e
+
+
+# Data visualization
+par(mfrow = c(1, 2))  
+# Plot heart.disease vs. biking
+plot(data$biking, data$heart.disease, 
+     pch = 16, col = "blue", 
+     xlab = "Biking", ylab = "Heart Disease", 
+     main = "Heart Disease vs. Biking")
+abline(lm(heart.disease ~ biking, data = data), col = "red", lwd = 2)
+
+# Plot heart.disease vs. smoking
+plot(data$smoking, data$heart.disease, 
+     pch = 16, col = "blue", 
+     xlab = "Smoking", ylab = "Heart Disease", 
+     main = "Heart Disease vs. Smoking")
+abline(lm(heart.disease ~ smoking, data = data), col = "red", lwd = 2)
+
+
+# Assumptions -------------------------------------------------------------
+# 1. Normality of Residuals
+shapiro.test(residuals(mod))
+
+# Q-Q Plot
+qqnorm(residuals(mod))
+qqline(residuals(mod), col = "red", lwd = 2)
+
+# 2. Homoscedasticity
+# The variance of residuals should be constant across fitted values.
+library(lmtest)
+bptest(mod)
+
+# Residuals vs. Fitted again for homoscedasticity
+plot(mod$fitted.values, residuals(mod), 
+     xlab = "Fitted Values", ylab = "Residuals", 
+     main = "Check for Homoscedasticity")
+abline(h = 0, col = "red", lwd = 2)
+
+
+
+# 3. Multicollinearity
+Predictors should not be too highly correlated with each other.
+Calculate Variance Inflation Factor (VIF) using the car package.
+
+vif(mod)
